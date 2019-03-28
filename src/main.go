1b9c02067fc780a4a0d6f9e4d3fb62739ceca8ab
alexis <alexis.auriac@epitech.eu>
2019-03-28 22:59:12 +0100

    Formatted code with gofmt

--- src/main.go
+++ src/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"math"
+	"os"
 )
 
 func duckReturnProb(a float64, t uint) float64 {
-	return (a*math.Exp(-float64(t)) + (4-3*a)*math.Exp(-2*float64(t)) + (2*a-4)*math.Exp(-4*float64(t)))
+	return (a*math.Exp(-float64(t)) +
+		(4-3*a)*math.Exp(-2*float64(t)) +
+		(2*a-4)*math.Exp(-4*float64(t)))
 }
 
 func primDuckReturnProb(a float64, t uint) float64 {
-	return (-a*math.Exp(-float64(t)) - (4-3*a)/2*math.Exp(-2*float64(t)) - (2*a-4)/4*math.Exp(-4*float64(t)))
+	return (-a*math.Exp(-float64(t)) -
+		(4-3*a)/2*math.Exp(-2*float64(t)) -
+		(2*a-4)/4*math.Exp(-4*float64(t)))
 }
 
 func probDensity(a float64, t uint) float64 {
@@ -21,6 +25,8 @@ func probDensity(a float64, t uint) float64 {
 func main() {
 	var a float64 = parseArgv(os.Args[1:])
 
-	fmt.Printf("Percentage of ducks back after 1 minute: %.1f\n", probDensity(a, 1))
-	fmt.Printf("Percentage of ducks back after 2 minute: %.1f%%\n", probDensity(a, 2))
+	fmt.Printf("Percentage of ducks back after 1 minute: %.1f\n",
+		probDensity(a, 1))
+	fmt.Printf("Percentage of ducks back after 2 minute: %.1f%%\n",
+		probDensity(a, 2))
 }
