17ee775968e91612ef7d4ee259f649683ebe0055
alexis <alexis.auriac@epitech.eu>
2019-03-28 22:56:55 +0100

    Percentage of ducks back after t time (line 5-6) done

--- src/main.go
+++ src/main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"math"
 )
+
+func duckReturnProb(a float64, t uint) float64 {
+	return (a*math.Exp(-float64(t)) + (4-3*a)*math.Exp(-2*float64(t)) + (2*a-4)*math.Exp(-4*float64(t)))
+}
+
+func primDuckReturnProb(a float64, t uint) float64 {
+	return (-a*math.Exp(-float64(t)) - (4-3*a)/2*math.Exp(-2*float64(t)) - (2*a-4)/4*math.Exp(-4*float64(t)))
+}
+
+func probDensity(a float64, t uint) float64 {
+	return (primDuckReturnProb(a, t) - primDuckReturnProb(a, 0)) * 100
+}
 
 func main() {
 	var a float64 = parseArgv(os.Args[1:])
 
-	fmt.Printf("Percentage of ducks back after 1 minute: %.1f\n", duckPercentage(a, 1))
-	fmt.Printf("Percentage of ducks back after 2 minute: %.1f\n", duckPercentage(a, 2))
+	fmt.Printf("Percentage of ducks back after 1 minute: %.1f\n", probDensity(a, 1))
+	fmt.Printf("Percentage of ducks back after 2 minute: %.1f%%\n", probDensity(a, 2))
 }
--- src/utility.go
+++ src/utility.go
@@ -1,7 +1,7 @@
 package main
 
-import "math"
+// import "math"
 
-func duckPercentage(a float64, t uint) (p float64) {
-	return (a*math.Exp(-float64(t)) + (4-3*a)*math.Exp(-2*float64(t)) + (2*a-4)*math.Exp(-4*float64(t))) * 100
-}
+// func duckPercentage(a float64, t uint) (p float64) {
+// 	return (a*math.Exp(-float64(t)) + (4-3*a)*math.Exp(-2*float64(t)) + (2*a-4)*math.Exp(-4*float64(t))) * 100
+// }
